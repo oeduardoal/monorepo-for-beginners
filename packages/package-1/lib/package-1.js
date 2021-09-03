b523247a1c1467f0f706a7ef13395ef2ad592abf
Eduardo <oeduardoal@gmail.com>
2021-09-03 16:04:21 -0300

    feat: 🎸 pkg fix

--- packages/package-1/lib/package-1.js
+++ packages/package-1/lib/package-1.js
@@ -4,8 +4,8 @@ module.exports = Package1;
 
 function Package1() {
   return {
-    show: function () {
-      console.log("show package-1 frontend breaking change");
+    show: function() {
+      console.log("showwww package-1 frontend breaking change");
     },
   };
 }
