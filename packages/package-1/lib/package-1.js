d87d29c621599db4f36f91bc6bfa857341d974d9
Eduardo <oeduardoal@gmail.com>
2021-09-13 16:15:41 -0300

    fix: 🐛 pacakge 1

--- packages/package-1/lib/package-1.js
+++ packages/package-1/lib/package-1.js
@@ -5,7 +5,7 @@ module.exports = Package1;
 function Package1() {
   return {
     show: function() {
-      console.log("showwww package-1 frontend breaking change");
+      console.log("showwww package-1 frontend breaking changess");
     },
   };
 }
